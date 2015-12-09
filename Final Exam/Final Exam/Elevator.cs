a32e2ef2c91ca1c3e73067c669a56371b83dce82
hclapp1 <harry_s_clapp@microsoft.com>
2015-12-08 22:08:22 -0500

    final exam

---
+++ Final Exam/Final Exam/Elevator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Exam
+{
+    class Elevator
+    {
+        private double MaxWeight;
+        private int MaxOccupants;
+        Passenger[] Occupants;
+        
+        
+
+        public Elevator(int maxOccupants, double maxWeight)
+        {
+            Occupants = new Passenger[maxOccupants];
+
+            MaxWeight = maxWeight;
+
+            MaxOccupants = maxOccupants;
+        }
+                
+    public void AddOccupants(Passenger passenger, int index)
+        {
+            Occupants[index] = passenger;
+        }
+
+
+
+        public double GetCurrentWeight()
+        {
+            double tWeight = 0;
+
+            foreach (Passenger ppl in Occupants)
+            {
+                tWeight += ppl.GetWeight();
+            }
+
+            return tWeight;
+        }
+        
+        public bool IsOverMaxCapacity()
+        {
+            if (GetCurrentWeight() >= MaxWeight)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }   
+        }
+    }
+}
---
+++ Final Exam/Final Exam/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool elevator1IsOverMaxCapacity;
+
+            Elevator elevator1;
+
+            elevator1 = new Elevator(2, 400);
+
+            Passenger person1 = new Passenger("A1", 180);
+
+            elevator1.AddOccupants(person1, 0);
+
+            Passenger person2 = new Passenger("A2", 200);
+
+            elevator1.AddOccupants(person2, 1);
+
+          
+
+            elevator1IsOverMaxCapacity = elevator1.IsOverMaxCapacity();
+
+
+            bool elevator2IsOverMaxCapacity;
+
+            Elevator elevator2;
+
+            elevator2 = new Elevator(3, 600);
+
+            Passenger person3 = new Passenger("A1", 200);
+
+            elevator2.AddOccupants(person3, 0);
+
+            Passenger person4 = new Passenger("A2", 200);
+
+            elevator2.AddOccupants(person4, 1);
+
+            Passenger person5 = new Passenger("A3", 201);
+
+            elevator2.AddOccupants(person5, 2);
+
+            
+
+            elevator2IsOverMaxCapacity = elevator2.IsOverMaxCapacity();
+            
+        }
+    }
+}
